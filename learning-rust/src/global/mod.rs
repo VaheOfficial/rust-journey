20e4621642a11f4865781a47c2a5fedf8f791303
VaheOfficial <vkho5466@gmail.com>
2024-08-19 15:56:49 -0700

    Added MSFVenom Payload Creation

--- learning-rust/src/global/mod.rs
+++ learning-rust/src/global/mod.rs
@@ -1,2 +1,4 @@
 pub mod input_acceptor;
 pub mod io_helper;
+pub mod msfconsole;
+pub mod input_helper;
--- learning-rust/src/main.rs
+++ learning-rust/src/main.rs
@@ -1,13 +1,16 @@
 mod calculator;  // Import the calculator module
 mod global;      // Import the global module
 mod youtube_downloader; //Import youtube downloader module
+mod malware_creator;
 
 fn main() {
     loop {
         println!("Choose an option:");
         println!("1. Calculator");
         println!("2. Youtube Video Downloader");
-        println!("3. Exit");
+        println!("3. Create a hacking executable");
+        println!("4. Exit");
+
 
         let choice = global::input_acceptor::read_input().trim().to_string();
 
@@ -16,10 +19,13 @@ fn main() {
                 calculator::start_calculator();  // Call the calculator function
             }
             "2" => {
-                youtube_downloader::yt_downloader();
-                break;
+                youtube_downloader::yt_downloader(); 
             }
             "3" => {
+               malware_creator::create_malware();  
+                break;
+            }
+            "4" => {
                 println!("Exiting...");
                 break;
             }
